ea7fbc41592b33d70b6c4935463bb6f62e16d545
lorel <lor.els@gmx.de>
2025-05-03 23:36:45 +0200

    Changed one Constructor

--- src/main/java/de/lorenz/restfullapi/dto/UserCoinsResponse.java
+++ src/main/java/de/lorenz/restfullapi/dto/UserCoinsResponse.java
@@ -1,18 +1,15 @@
 // dto/UserCoinsResponse.java
 package de.lorenz.restfullapi.dto;
 
-import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@AllArgsConstructor
 public class UserCoinsResponse {
 
+    @Getter
     private String uuid;
+    @Getter
     private int coins;
 
-    public UserCoinsResponse(String uuid, int coins) {
-        this.uuid = uuid;
-        this.coins = coins;
-    }
-
-    public String getUuid() { return uuid; }
-    public int getCoins() { return coins; }
 }
