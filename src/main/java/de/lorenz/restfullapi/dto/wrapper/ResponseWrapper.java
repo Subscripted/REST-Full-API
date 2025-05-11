1dd9cba2b40f8b7d6c22391600a93db92e0cffab
lorel <lor.els@gmx.de>
2025-05-11 08:11:57 +0200

    Added a besser ResponseWrapper

--- src/main/java/de/lorenz/restfullapi/dto/wrapper/ResponseWrapper.java
+++ src/main/java/de/lorenz/restfullapi/dto/wrapper/ResponseWrapper.java
@@ -1,25 +1,49 @@
 package de.lorenz.restfullapi.dto.wrapper;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 import java.util.Collection;
 
-@RequiredArgsConstructor
+@Getter
 public class ResponseWrapper<T> {
 
-    @Getter
-    private T response;
+    private final T response;
+    private final String message;
+    private final String statusCode;
+    private final int count;
 
-    @Getter
-    private int count;
-
-    public ResponseWrapper(T response) {
+    private ResponseWrapper(T response, String message, String statusCode) {
         this.response = response;
+        this.message = message;
+        this.statusCode = statusCode;
         if (response instanceof Collection<?>) {
             this.count = ((Collection<?>) response).size();
         } else {
-            this.count = 1;
+            this.count = response != null ? 1 : 0;
         }
+    }
+
+    public static <T> ResponseWrapper<T> ok(T data) {
+        return new ResponseWrapper<>(data, "OK", "200");
+    }
+
+    public static <T> ResponseWrapper<T> ok(T data, String message) {
+        return new ResponseWrapper<>(data, message, "200");
+    }
+
+    public static <T> ResponseWrapper<T> error(String message) {
+        return new ResponseWrapper<>(null, message, "500");
+    }
+
+    public static <T> ResponseWrapper<T> error(T data, String message) {
+        return new ResponseWrapper<>(data, message, "500");
+    }
+
+    public static <T> ResponseWrapper<T> withStatus(T data, String message, String statusCode) {
+        return new ResponseWrapper<>(data, message, statusCode);
+    }
+
+    public static <T> ResponseWrapper<T> empty() {
+        return new ResponseWrapper<>(null, "No Content", "204");
     }
 }
