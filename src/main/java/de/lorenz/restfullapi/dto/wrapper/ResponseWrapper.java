fd10197c7769a9ec0d5e21e03de298c3fef08036
lorel <lor.els@gmx.de>
2025-05-15 22:14:19 +0200

    feat(JsonFormat): Changed some Respones
    The Responses of UserController now got a more specific message

--- src/main/java/de/lorenz/restfullapi/dto/wrapper/ResponseWrapper.java
+++ src/main/java/de/lorenz/restfullapi/dto/wrapper/ResponseWrapper.java
@@ -1,5 +1,6 @@
 package de.lorenz.restfullapi.dto.wrapper;
 
+import de.lorenz.restfullapi.global.exception.GlobalHttpStatusCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -26,26 +27,51 @@ public class ResponseWrapper<T> {
     }
 
     public static <T> ResponseWrapper<T> ok(T data) {
-        return new ResponseWrapper<>(data, "OK", "200");
+        return new ResponseWrapper<>(data, "OK", GlobalHttpStatusCode.OK.getCode());
     }
 
     public static <T> ResponseWrapper<T> ok(T data, String message) {
-        return new ResponseWrapper<>(data, message, "200");
+        return new ResponseWrapper<>(data, message, GlobalHttpStatusCode.OK.getCode());
     }
 
     public static <T> ResponseWrapper<T> error(String message) {
-        return new ResponseWrapper<>(null, message, "500");
+        return new ResponseWrapper<>(null, message, GlobalHttpStatusCode.INTERNAL_ERROR.getCode());
     }
 
     public static <T> ResponseWrapper<T> error(T data, String message) {
-        return new ResponseWrapper<>(data, message, "500");
+        return new ResponseWrapper<>(data, message, GlobalHttpStatusCode.INTERNAL_ERROR.getCode());
     }
 
-    public static <T> ResponseWrapper<T> withStatus(T data, String message, String statusCode) {
-        return new ResponseWrapper<>(data, message, statusCode);
+    public static <T> ResponseWrapper<T> badRequest(String message) {
+        return new ResponseWrapper<>(null, message, GlobalHttpStatusCode.BAD_REQUEST.getCode());
     }
 
-    public static <T> ResponseWrapper<T> empty() {
-        return new ResponseWrapper<>(null, "No Content", "204");
+    public static <T> ResponseWrapper<T> badRequest(T data, String message) {
+        return new ResponseWrapper<>(data, message, GlobalHttpStatusCode.BAD_REQUEST.getCode());
     }
+
+    public static <T> ResponseWrapper<T> unauthorized(String message) {
+        return new ResponseWrapper<>(null, message, GlobalHttpStatusCode.UNAUTHORIZED.getCode());
+    }
+
+    public static <T> ResponseWrapper<T> unauthorized(T data, String message) {
+        return new ResponseWrapper<>(data, message, GlobalHttpStatusCode.UNAUTHORIZED.getCode());
+    }
+
+    public static <T> ResponseWrapper<T> forbidden(String message) {
+        return new ResponseWrapper<>(null, message, GlobalHttpStatusCode.FORBIDDEN.getCode());
+    }
+
+    public static <T> ResponseWrapper<T> forbidden(T data, String message) {
+        return new ResponseWrapper<>(data, message, GlobalHttpStatusCode.FORBIDDEN.getCode());
+    }
+
+    public static <T> ResponseWrapper<T> notFound(String message) {
+        return new ResponseWrapper<>(null, message, GlobalHttpStatusCode.NOT_FOUND.getCode());
+    }
+
+    public static <T> ResponseWrapper<T> notFound(T data, String message) {
+        return new ResponseWrapper<>(data, message, GlobalHttpStatusCode.NOT_FOUND.getCode());
+    }
+
 }
