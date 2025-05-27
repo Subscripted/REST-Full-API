5370eafce9c624d4fb6c3bb3bc4684383924c6b9
Lorenz <lor.els@gmx.de>
2025-05-27 22:59:16 +0200

    Added a Blocked Field for further Manipulation of a User

--- src/main/documentation/logincreds.sql
+++ src/main/documentation/logincreds.sql
@@ -3,5 +3,6 @@ create table logincreds
     email         varchar(255) null,
     password      varchar(200) null,
     client_id     varchar(255) null,
-    client_secret varchar(255) null
+    client_secret varchar(255) null,
+    blocked       tinyint      null
 );
